f7cf6be7894ff3270b3f40ecfe8c1593471d0846
Ivan JB <ivanjbdesign@gmail.com>
2017-04-25 00:27:03 -0400

    Added @import, _global.css, _large-hero.css, & div .large-hero__text-content

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 gulp.task('default', function() {
@@ -16,7 +17,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssVars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 

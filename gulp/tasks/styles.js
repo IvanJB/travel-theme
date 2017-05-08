af0e03ddddd19bce5972a366900d1e8071b96e54
Ivan JB <ivanjbdesign@gmail.com>
2017-05-07 22:37:38 -0400

    media queries added to hero image & title font size for sml, med, lrg

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,13 +3,14 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 // Gulp task to inject selectors to temp style.css
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
         .on('error', function(errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end');
